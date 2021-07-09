a0e365f4d6571b91dfc2ab32036b044f57a96d51
Miguel Coder <syntacticsolutions@gmail.com>
2021-07-08 23:14:36 -0700

    Update backend

--- graphql/api/posts/index.js
+++ graphql/api/posts/index.js
@@ -1,94 +1,145 @@
-const db = require('../../database/mysql')
-const {errorHandler} = require('../utils')
-const intersection = require('lodash/intersection')
+const db = require("../../database/mysql");
+const { errorHandler } = require("../utils");
+const intersection = require("lodash/intersection");
 
 module.exports = {
-    getPost: async (id) => 
-        await db.select('*')
-            .from('blog_posts')
-            .where({ id })
-            .andWhere(function() {
-                this.where('active', '=', 1)
-            })
-            .catch(errorHandler),
+  getPost: async (id) => {
+    const posts = db
+      .select("*")
+      .from("blog_posts")
+      .where({ id })
+      .catch(errorHandler);
 
-    createPost: async (args) => 
-        await db.insert(args.data)
-            .then(res => {
-                return res?.[0] || 0
-            }),
+    const comments = await db
+      .select("*")
+      .from("blog_post_comments")
+      .where({ post_id: id })
+      .catch(errorHandler);
 
-    getPosts: (type, category_ids) => {
-        let qry = db.select(
-                'blog_posts.id',
-                'blog_posts.description',
-                'blog_posts.author_id',
-                'title',
-                'image',
-                'created_at',
-                'updated_at',
-                db.raw('GROUP_CONCAT(label) as categories'),
-                db.raw('GROUP_CONCAT(blog_categories.id) as cat_ids')
-            )
-            .from('blog_posts')
-            .leftJoin('blog_post_categories', 'blog_posts.id', 'blog_post_categories.post_id')
-            .leftJoin('blog_categories', 'blog_post_categories.category_id', 'blog_categories.id')
-            .where({active: 1})
-            .groupBy('blog_posts.id')
-        
-        qry = {
-            trending: () =>  qry
-                .select(db.raw('COUNT(blog_post_likes.author_id) as likes'))
-                .leftJoin('blog_post_likes', 'blog_post_likes.post_id', 'blog_posts.id')
-                .groupBy('blog_posts.id')
-                .orderBy('likes','asc')
-                .limit(5),
+    let commentAuthorIds = [
+      ...new Set(comments.map(({ author_id }) => author_id)),
+    ];
 
-            featured: () => qry.whereIn('blog_posts.id', [1,2,3,4]),
+    let commentAuthors = await db
+      .select("*")
+      .from("users")
+      .whereIn("id", commentAuthorIds);
 
-            recent: () => qry
-                .orderBy('updated_at', 'desc')
-                .limit(5),
+    comments.forEach((comment) => {
+      let authorUser = commentAuthors.find(
+        ({ id }) => id === comment.author_id
+      );
 
-            default: () => qry
-        }[type || 'default']()
+      comment.author = `${authorUser.first_name} ${authorUser.last_name}`;
+      comment.author_image = authorUser.image;
+    });
 
-        return qry.then(data => {
-            if (category_ids) {
-                return data.filter((post) => 
-                    post.cat_ids &&
-                        intersection(
-                            post.cat_ids
-                                .split(',')
-                                .map(parseInt),
-                            category_ids
-                        ).length
-                )
-            }
-            return data
-        })
-        .catch(err => {
-            console.log(err)
-        })
+    let likes = db
+      .select("*")
+      .from("blog_post_likes")
+      .where({ post_id: id })
+      .catch(errorHandler);
 
-    },
-    
-    getPostsLikeCounts: async (ids) =>
-        await db.select('COUNT(author_id) as likes', 'post_id')
-            .from('blog_post_likes')
-            .groupBy('post_id')
-            .whereIn('post_id', ids)
-            .catch(errorHandler),
+    let categories = await db
+      .select("*")
+      .from("blog_post_categories")
+      .leftJoin("blog_categories", "category_id", "blog_categories.id")
+      .where({ post_id: id });
 
-    getPostComments: async (id) => 
-        await db.select('*')
-            .from('blog_post_comments')
-            .where('post_id', id)
-            .catch(errorHandler),
+    let [post] = await posts;
 
-    getPostsAuthors: async (ids) =>
-        await db.select('*')
-            .from('users')
-            .where('id', ids)
-            .catch(errorHandler)
-}
+    return {
+      ...post,
+      comments,
+      likes: await likes.length,
+      categories: await categories.map(({ label }) => label),
+    };
+  },
+
+  createPost: async (args) =>
+    await db.insert(args.data).then((res) => {
+      return res;
+    }),
+
+  getPosts: (type, category_ids) => {
+    let qry = db
+      .select(
+        "blog_posts.id",
+        "blog_posts.description",
+        "blog_posts.author_id",
+        "title",
+        "image",
+        "created_at",
+        "updated_at",
+        db.raw("GROUP_CONCAT(label) as categories"),
+        db.raw("GROUP_CONCAT(blog_categories.id) as cat_ids")
+      )
+      .from("blog_posts")
+      .leftJoin(
+        "blog_post_categories",
+        "blog_posts.id",
+        "blog_post_categories.post_id"
+      )
+      .leftJoin(
+        "blog_categories",
+        "blog_post_categories.category_id",
+        "blog_categories.id"
+      )
+      .where({ active: 1 })
+      .groupBy("blog_posts.id");
+
+    qry = {
+      trending: () =>
+        qry
+          .select(db.raw("COUNT(blog_post_likes.author_id) as likes"))
+          .leftJoin(
+            "blog_post_likes",
+            "blog_post_likes.post_id",
+            "blog_posts.id"
+          )
+          .groupBy("blog_posts.id")
+          .orderBy("likes", "asc")
+          .limit(5),
+
+      featured: () => qry.whereIn("blog_posts.id", [1, 2, 3, 4]),
+
+      recent: () => qry.orderBy("updated_at", "desc").limit(5),
+
+      default: () => qry,
+    }[type || "default"]();
+
+    return qry
+      .then((data) => {
+        if (category_ids) {
+          return data.filter(
+            (post) =>
+              post.cat_ids &&
+              intersection(post.cat_ids.split(",").map(parseInt), category_ids)
+                .length
+          );
+        }
+        return data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+
+  getPostsLikeCounts: async (ids) =>
+    await db
+      .select("COUNT(author_id) as likes", "post_id")
+      .from("blog_post_likes")
+      .groupBy("post_id")
+      .whereIn("post_id", ids)
+      .catch(errorHandler),
+
+  getPostComments: async (id) =>
+    await db
+      .select("*")
+      .from("blog_post_comments")
+      .where("post_id", id)
+      .catch(errorHandler),
+
+  getPostsAuthors: async (ids) =>
+    await db.select("*").from("users").where("id", ids).catch(errorHandler),
+};
--- graphql/schemas/posts/index.js
+++ graphql/schemas/posts/index.js
@@ -1,55 +1,52 @@
-const fs = require('fs')
-const path = require('path')
-const PostService = require('../../api/posts')
-const {errorHandler} = require('../../api/utils')
+const fs = require("fs");
+const path = require("path");
+const PostService = require("../../api/posts");
+const { errorHandler } = require("../../api/utils");
 
-const getPostsWithAuthors = cb => 
-    async (parent, args, context, info) => {
-        const posts = await cb(args, parent)
-            .catch(errorHandler)
+const getPostsWithAuthors = (cb) => async (parent, args, context, info) => {
+  const posts = await cb(args, parent).catch(errorHandler);
 
-        if (!posts.length) return []
+  if (!posts.length) return [];
 
-        const authors = await PostService.getPostsAuthors(
-            [...new Set(posts.map(({author_id}) => author_id))]
-        )
-        .catch(errorHandler)
+  const authors = await PostService.getPostsAuthors([
+    ...new Set(posts.map(({ author_id }) => author_id)),
+  ]).catch(errorHandler);
 
+  const authorMap = authors.reduce(
+    (map, author) => ({
+      ...map,
+      [author.id]: `${author.first_name} ${author.last_name}`,
+    }),
+    {}
+  );
 
-        const authorMap = authors.reduce((map, author) => ({
-            ...map,
-            [author.id]: `${author.first_name} ${author.last_name}`
-        }), {})
-
-        return posts.map(({categories, author_id, ...rest}, index) => 
-            ({
-                categories: categories.split(','),
-                ...rest,
-                author: authorMap[author_id]
-            })
-
-        )
-    }
+  return posts.map(({ categories, author_id, ...rest }, index) => ({
+    categories: categories.split(","),
+    ...rest,
+    author: authorMap[author_id],
+  }));
+};
 
 module.exports = {
-    resolvers: {
-        Query: {
-            getPostsByType: getPostsWithAuthors(
-                async ({type}) => await PostService.getPosts(type)
-            ),
-            getPostsByCategories: getPostsWithAuthors(
-                async ({cat_ids}) => await PostService.getPosts('default', cat_ids)
-            ),
-            getPost: async (parent, args) => PostService.getPost(args.id)
-        },
-        Mutation: {
-            createPost: async (parent, args) => PostService.createPost(args)
-        }
+  resolvers: {
+    Query: {
+      getPostsByType: getPostsWithAuthors(
+        async ({ type }) => await PostService.getPosts(type)
+      ),
+      getPostsByCategories: getPostsWithAuthors(
+        async ({ cat_ids }) => await PostService.getPosts("default", cat_ids)
+      ),
+      getPost: async (parent, { id }) => {
+        const post = await PostService.getPost(id);
+
+        return post;
+      },
     },
-    schema: fs.readFileSync(
-        path.resolve(
-            __dirname,
-            './posts-schema.graphql'
-        )
-    ).toString()
-}
+    Mutation: {
+      createPost: async (parent, args) => await PostService.createPost(args),
+    },
+  },
+  schema: fs
+    .readFileSync(path.resolve(__dirname, "./posts-schema.graphql"))
+    .toString(),
+};
